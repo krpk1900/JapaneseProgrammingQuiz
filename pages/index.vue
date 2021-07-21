64249a3041b35411fdd779027ac48a6f7fe40dec
krpk1900 <krpk1900@gmail.com>
2021-07-21 22:50:58 +0900

    [Add] モーダルの挙動を確認

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <p>Hello World</p>
+  <v-dialog v-model="isShowDialog" width="510" persistent>
+    <template v-slot:activator="{ on, attrs }">
+      <a v-bind="attrs" v-on="on" style="color: #099e9e;">　</a>
+    </template>
+    <v-card>
+      test
+    </v-card>
+  </v-dialog>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isShowDialog: true,
+  })
+}
+</script>
