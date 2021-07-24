111d3461f4856fe91b85524a609c19ee7414c92e
krpk1900 <krpk1900@gmail.com>
2021-07-24 17:51:33 +0900

    [Add] 問題を12問すべて実装

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,25 @@
     <QuizCard v-if="isShowDialogs2" @next="isShowDialogs3 = true"
     questionNumber="2" questionText="碁" answerText="Go" imageName="go.jpeg"></QuizCard>
     <QuizCard v-if="isShowDialogs3" @next="isShowDialogs4 = true"
-    questionNumber="3" questionText="絵里www" answerText="elixir (エリクサー)" imageName="elixir.png"></QuizCard>
+    questionNumber="3" questionText="絵里www" answerText="elixir (エリクサ)" imageName="elixir.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs4" @next="isShowDialogs5 = true"
+    questionNumber="4" questionText="古墓流" answerText="COBOL (コボル)" imageName="cobol.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs5" @next="isShowDialogs6 = true"
+    questionNumber="5" questionText="反応する" answerText="React" imageName="react.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs6" @next="isShowDialogs7 = true"
+    questionNumber="6" questionText="次" answerText="Next.js" imageName="nextjs.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs7" @next="isShowDialogs8 = true"
+    questionNumber="7" questionText="炎上してしまった基地" answerText="Firebase" imageName="firebase.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs8" @next="isShowDialogs9 = true"
+    questionNumber="8" questionText="濃度" answerText="Node.js" imageName="nodejs.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs9" @next="isShowDialogs10 = true"
+    questionNumber="9" questionText="海言語" answerText="C言語" imageName="c.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs10" @next="isShowDialogs11 = true"
+    questionNumber="10" questionText="凄価羅" answerText="Scala (スカラ)" imageName="scala.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs11" @next="isShowDialogs12 = true"
+    questionNumber="11" questionText="凄い記述を作り上げる言語" answerText="HTML (HyperTextMarkupLanguage)" imageName="html.png"></QuizCard>
+    <QuizCard v-if="isShowDialogs12" @next="isShowDialogs12 = true"
+    questionNumber="12" questionText="乳尊" answerText="Python (パイソン)" imageName="python.png"></QuizCard>
   </div>
 </template>
 
