62e4f19121464e83c6651a622e363e0863b723e3
krpk1900 <krpk1900@gmail.com>
2021-07-27 23:56:47 +0900

    [Modify] カードの横幅を拡大

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="isShowDialog" width="520" persistent>
+  <v-dialog v-model="isShowDialog" width="540" persistent>
     <template v-slot:activator="{ on, attrs }">
       <a v-bind="attrs" v-on="on" style="color: #099e9e;">　</a>
     </template>
