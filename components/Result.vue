ad48dc64c8981dc9bcb683fb717f34d27bd43c1c
krpk1900 <krpk1900@gmail.com>
2021-07-27 23:57:31 +0900

    [Add] メッセージを動的に変更

--- components/Result.vue
+++ components/Result.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-dialog v-model="isShowDialog" width="520" persistent>
+  <v-dialog v-model="isShowDialog" width="540" persistent>
     <template v-slot:activator="{ on, attrs }">
       <a v-bind="attrs" v-on="on" style="color: #099e9e;">　</a>
     </template>
     <v-card>
       <div class="card cyan darken-1 white--text">
         <div class="question text-h6 text-md-h4 font-weight-black">あなたの点数は
-          <span class="text-h4 text-md-h2 font-weight-black">{{ this.$store.state.score }}</span>/12点
+          <span class="text-h4 text-md-h2 font-weight-black">{{ this.$store.state.score }}</span>/12点でした！
         </div>
-        <div>
-          {{ level }}レベルです！
+        <div class="text-center text-h6 font-weight-bold">
+          {{ this.message }}
         </div>
 
         <v-card-actions>
@@ -28,25 +28,26 @@ export default Vue.extend({
   data() {
     return{
       isShowDialog: true as boolean,
-      level: "デフォルト" as string,
+      message: "デフォルトメッセージ" as string,
     }
   },
   methods: {
     shareMessage():string {
-      return `https://twitter.com/share?url=https://perfect-security.jp/%0a&via=krpk1900_dev&related=krpk1900_dev&hashtags=世界一セキュリティが堅牢なアプリ,クソアプリ%0a&text=あなたは【世界一堅牢なセキュリティ】を突破した【】人目の凄腕ハッカーです！%0a`
+      return `https://twitter.com/share?url=https://perfect-security.jp/%0a&via=krpk1900_dev&related=krpk1900_dev&hashtags=日本人のためのプログラミングクイズ,クソアプリ%0a&text=あなたの点数は${this.$store.state.score}点でした！%0aCTOへの道まではあと${12-this.$store.state.score}点です！%0a`
     },
   },
   props: {
   },
   created: function(){
     console.log(`created`)
-    switch(this.$store.state.score){
-      case 12:
-        console.log(`12点`)
-        break;
-      case 11:
-        console.log(`11点`)
-        break;
+    if( this.$store.state.score == 12){
+      this.message = "CTOレベルです！"
+    } else if(10 <= this.$store.state.score && this.$store.state.score <= 11){
+      this.message = "CTOレベルまであと5年はかかるでしょう…"
+    } else if(8 <= this.$store.state.score && this.$store.state.score <= 9){
+      this.message = "CTOレベルまであと10年はかかるでしょう…"
+    } else if(0 <= this.$store.state.score && this.$store.state.score <= 7){
+      this.message = "CTOレベルまであと20年はかかるでしょう…"
     }
   }
 })
