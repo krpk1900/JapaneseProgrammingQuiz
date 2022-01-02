8b6c0464681c9a69e151efc831ea08493b3b567e
krpk1900 <krpk1900@gmail.com>
2022-01-03 01:41:35 +0900

    [Fix] カードの色を変更

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -4,14 +4,14 @@
       <a v-bind="attrs" v-on="on" style="color: #099e9e;">　</a>
     </template>
     <v-card>
-      <div class="card cyan darken-1 white--text">
+      <div class="card red darken-4 black--text">
         <div class="question text-h6 text-md-h4 font-weight-black">Webエンジニアのくせに<br>全問解けねぇ奴いる？</div>
         <div class="img">
           <img src="touribe.png" height="400px">
           <div class="text-caption">Ⓒ和久井健・講談社／<br>アニメ「東京リベンジャーズ」製作委員会</div>
         </div>
         <v-card-actions>
-          <v-btn class="btn btn-next cyan white--text text-h6 text-md-h4 font-weight-black" width=250px
+          <v-btn class="btn btn-next grey lighten-5 black--text text-h6 text-md-h4 font-weight-black" width=250px
           @click="start" elevation="2">全問潰すぞ!!!</v-btn>
         </v-card-actions>
       </div>
