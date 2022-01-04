cd37e06111d42ba2c16be0e4ba5d2d91588a292f
krpk1900 <krpk1900@gmail.com>
2022-01-05 01:59:37 +0900

    [Add] メタデータを設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,13 +12,20 @@ export default {
     //titleTemplate: '%s - JapaneseProgrammingQuiz',
     title: 'Webエンジニアのくせに解けねぇ奴いる？',
     htmlAttrs: {
-      lang: 'ja'
+      lang: 'ja',
+      prefix: 'og: http://ogp.me/ns#'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Webエンジニアなら解けるプログラミングなぞなぞです。' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Webエンジニアのくせに解けねぇ奴いる？' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://programming-quiz.com/' },
+      { hid: 'og:title', property: 'og:title', content: 'Webエンジニアのくせに解けねぇ奴いる？' },
+      { hid: 'og:description', property: 'og:description', content: 'Webエンジニアなら解けるプログラミングなぞなぞです。' },
+      { hid: 'og:image', property: 'og:image', content: 'https://programming-quiz.com/ogp.png' },
+      { name: 'twitter:card', content: 'summary' },　//twitterの画像サイズ summary_large_image
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
